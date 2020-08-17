72cb653adea2caa56c71e4c2765e65b2248140ff
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-08-17 23:02:12 +0530

    add method to change the check out status of a video.

--- src/definitions/Video.java
+++ src/definitions/Video.java
@@ -10,11 +10,20 @@ public class Video {
     private boolean checkOut;
     private int rating;
 
-    /*
-     *This method returns the name of the video.
+    /**
+     * This method returns the name of the video.
+     *
      * @return Then name of the video
-     * */
+     */
     public String getName() {
         return "";
+    }
+
+    /**
+     * This method sets the checkout value of a video to true or false.
+     * If the value is set to true, that means that a customer has rented a video.
+     */
+    public void checkOut() {
+
     }
 }
