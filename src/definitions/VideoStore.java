cd38fc95e4d9ca8dd6318e1ac97c554ac0c02e15
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-08-20 23:01:27 +0530

    create constructors for VideoStore.java

--- src/definitions/VideoStore.java
+++ src/definitions/VideoStore.java
@@ -8,6 +8,13 @@ package definitions;
 public class VideoStore {
     private Video[] store;
 
+    public VideoStore() {
+        this.store = new Video[10];
+        for (int i = 0; i < store.length; i++) {
+            store[i] = new Video();
+        }
+    }
+
     public Video[] getStore() {
         return store.clone();
     }
